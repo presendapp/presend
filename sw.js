cd809827f2b19a2be3d29138140141e77e77a9a7
presendapp <presendapp@gmail.com>
2026-08-15 00:08:38 +0200

    feat: add PWA support (service worker + manifest) for offline use and installability

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'presend-v1';
+const STATIC_ASSETS = [
+  '/',
+  '/style.css',
+  '/favicon.ico',
+  '/favicon-96x96.png',
+  '/apple-touch-icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
