98bc88aae3991df3057195d3412c7144a7b4644a
presendapp <presendapp@gmail.com>
2026-08-16 09:42:49 +0200

    perf(core-web-vitals): defer redirect script, add service worker cache, FAQ rich snippets on guides

--- sw.js
+++ sw.js
@@ -3,21 +3,21 @@ const STATIC_ASSETS = [
   '/',
   '/style.css',
   '/favicon.ico',
-  '/favicon-96x96.png',
-  '/apple-touch-icon.png'
+  '/manifest.json'
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS))
-  );
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(STATIC_ASSETS)));
   self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(
+    keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+  )));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
