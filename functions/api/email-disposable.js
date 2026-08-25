ed4a85004ba35d67c6c0d1084288ae59dcc47bc7
presendapp <presendapp@gmail.com>
2026-08-25 13:14:54 +0200

    feat: nouveaux endpoints /api/password-breach (HIBP k-anonymity) et /api/email-disposable

---
+++ functions/api/email-disposable.js
@@ -0,0 +1,64 @@
+// GET /api/email-disposable?email=foo@mailinator.com
+// Detects disposable/throwaway email providers from a curated list of 576 known domains.
+// Same list used by the client-side tool (tools/disposable-email-checker.html).
+
+async function checkRateLimit(env, clientIP, bucket) {
+  if (!env.PRESEND_ANALYTICS) return true;
+  const now = Math.floor(Date.now() / 60000);
+  const rateKey = `rate:${bucket}:${clientIP}:${now}`;
+  let count = await env.PRESEND_ANALYTICS.get(rateKey);
+  count = count ? parseInt(count) : 0;
+  if (count >= 60) return false;
+  await env.PRESEND_ANALYTICS.put(rateKey, (count + 1).toString(), { expirationTtl: 120 });
+  return true;
+}
+
+function corsHeaders(extra = {}) {
+  return { 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Methods': 'GET, OPTIONS', ...extra };
+}
+
+const DISPOSABLE_DOMAINS = new Set(["10minemail.com", "10minutemail.com", "10minutemail.net", "33mail.com", "anonbox.net", "anonymbox.com", "binkmail.com", "bobmail.info", "bugmenot.com", "burnermail.io", "byom.de", "chammy.info", "childsavetrust.org", "chogmail.com", "cool.fr.nf", "correo.blogos.net", "cosmorph.com", "courriel.fr.nf", "courrieltemporaire.com", "crazymailing.com", "cust.in", "dacoolest.com", "dandikmail.com", "dayrep.com", "deadaddress.com", "despammed.com", "devnullmail.com", "dfgh.net", "digitalsanctuary.com", "dingbone.com", "discard.email", "discardmail.com", "discardmail.de", "disposableaddress.com", "disposableemailaddresses.com", "disposableinbox.com", "dispostable.com", "dodgeit.com", "dodgit.com", "dodgit.org", "donemail.ru", "dontreg.com", "dontsendmespam.de", "dump-email.info", "dumpandjunk.com", "dumpmail.de", "dumpyemail.com", "e4ward.com", "email60.com", "emaildienst.de", "emailfake.com", "emailias.com", "emailigo.de", "emailinfive.com", "emailmiser.com", "emailondeck.com", "emailsensei.com", "emailtemporario.com.br", "emailwarden.com", "emailx.at.hm", "emailxfer.com", "emeil.in", "emeil.ir", "emz.net", "enterto.com", "ephemail.net", "etranquil.com", "etranquil.net", "etranquil.org", "evopo.com", "explodemail.com", "fake-mail.net", "fakeinbox.com", "fakemail.net", "fakemailgenerator.com", "fakemailz.com", "fantasymail.de", "fastacura.com", "fastchevy.com", "fastchrysler.com", "fastkawasaki.com", "fastmazda.com", "fastmitsubishi.com", "fastnissan.com", "fastsubaru.com", "fastsuzuki.com", "fasttoyota.com", "fastyamaha.com", "fatflap.com", "fdfdsfds.com", "fightallspam.com", "filzmail.com", "fizmail.com", "fleckens.hu", "frapmail.com", "freundin.ru", "front14.org", "fux0ringduh.com", "garliclife.com", "gehensiemirnichtaufdensack.de", "get1mail.com", "get2mail.fr", "getairmail.com", "getnada.com", "getonemail.com", "ghosttexter.de", "girlsundertheinfluence.com", "gishpuppy.com", "gmial.com", "gowikibooks.com", "gowikicampus.com", "gowikicars.com", "gowikifilms.com", "gowikigames.com", "gowikimusic.com", "gowikinetwork.com", "gowikitravel.com", "gowikitv.com", "grandmamail.com", "great-host.in", "greensloth.com", "grr.la", "guerillamail.biz", "guerillamail.com", "guerillamail.net", "guerillamail.org", "guerrillamail.biz", "guerrillamail.com", "guerrillamail.de", "guerrillamail.net", "guerrillamail.org", "guerrillamailblock.com", "h.mintemail.com", "h8s.org", "hacccc.com", "haltospam.com", "harakirimail.com", "harakirimail.info", "hidemail.de", "hidzz.com", "hmamail.com", "hopemail.biz", "ieh-mail.de", "ikbenspamvrij.nl", "imails.info", "inbax.tk", "inbound.plus", "inboxalias.com", "inboxclean.com", "inboxclean.org", "incognitomail.com", "infocom.zp.ua", "instant-mail.de", "ipoo.org", "irish2me.com", "iwi.net", "jetable.com", "jetable.fr.nf", "jetable.net", "jetable.org", "jourrapide.com", "jsrsolutions.com", "kasmail.com", "kaspop.com", "keepmymail.com", "killmail.com", "killmail.net", "klassmaster.com", "klzlk.com", "koszmail.pl", "kurzepost.de", "lawlita.com", "letthemeatspam.com", "lhsdv.com", "lifebyfood.com", "link2mail.net", "litedrop.com", "lookugly.com", "lopl.co.cc", "lortemail.dk", "lroid.com", "lukop.dk", "m4ilweb.info", "mail-filter.com", "mail-temp.com", "mail-temporaire.fr", "mail.by", "mail.mezimages.net", "mail1a.de", "mail21.cc", "mail2rss.org", "mail333.com", "mail4trash.com", "mailbidon.com", "mailblocks.com", "mailbucket.org", "mailcat.biz", "mailcatch.com", "mailde.de", "mailde.info", "maildrop.cc", "maileater.com", "mailexpire.com", "mailfa.tk", "mailforspam.com", "mailfreeonline.com", "mailguard.me", "mailimate.com", "mailin8r.com", "mailinater.com", "mailinator.com", "mailinator.net", "mailinator.org", "mailmate.com", "mailme.gq", "mailme.lv", "mailmetrash.com", "mailmoat.com", "mailms.com", "mailnesia.com", "mailnull.com", "mailorg.org", "mailpick.biz", "mailrock.biz", "mailsac.com", "mailscrap.com", "mailshell.com", "mailsiphon.com", "mailslapping.com", "mailslite.com", "mailtemp.info", "mailtome.de", "mailtothis.com", "mailtrash.net", "mailtv.net", "mailtv.tv", "mailzilla.com", "mailzilla.org", "makemetheking.com", "manybrain.com", "mbx.cc", "mciek.com", "mega.zik.dj", "meinspamschutz.de", "meltmail.com", "messagebeamer.de", "mierdamail.com", "mintemail.com", "moakt.cc", "moakt.com", "moburl.com", "mohmal.com", "mohmal.in", "monemail.fr.nf", "monmail.fr.nf", "monumentmail.com", "msa.minsmail.com", "mt2009.com", "mt2014.com", "mt2015.com", "mycard.net.ua", "mycleaninbox.net", "mypartyclip.de", "myspaceinc.com", "myspaceinc.net", "myspaceinc.org", "myspacepimpedup.com", "myspamless.com", "mytemp.email", "mytrashmail.com", "nada.email", "neomailbox.com", "nepwk.com", "nervmich.net", "nervtmich.net", "netmails.com", "netmails.net", "netzidiot.de", "neverbox.com", "nice-4u.com", "nincsmail.hu", "nnh.com", "no-spam.ws", "noclickemail.com", "nomail.pw", "nomail2me.com", "nomorespamemails.com", "nospam.ze.tc", "nospam4.us", "nospamfor.us", "nospammail.net", "notmailinator.com", "nowmymail.com", "nurfuerspam.de", "nus.edu.sg", "nwldx.com", "objectmail.com", "obobbo.com", "odaymail.com", "oneoffemail.com", "onewaymail.com", "onlatedotcom.info", "opayq.com", "ordinaryamerican.net", "otherinbox.com", "ovpn.to", "owlpic.com", "pancakemail.com", "politikerclub.de", "poofy.org", "pookmail.com", "privacy.net", "proxymail.eu", "prtnx.com", "punkass.com", "putthisinyourspamdatabase.com", "qq.com", "quickinbox.com", "rcpt.at", "reallymymail.com", "realtyalerts.ca", "recode.me", "recursor.net", "regbypass.com", "regbypass.comsafe-mail.net", "rejectmail.com", "reveall.info", "rmqkr.net", "rppkn.com", "rtrtr.com", "s0ny.net", "safe-mail.net", "safersignup.de", "safetymail.info", "safetypost.de", "sandelf.de", "saynotospams.com", "selfdestructingmail.com", "sendspamhere.com", "sharklasers.com", "shieldedmail.com", "shitmail.me", "shitware.nl", "shortmail.net", "sibmail.com", "sinnlos-mail.de", "siteposter.net", "skeefmail.com", "slaskpost.se", "slopsbox.com", "smashmail.de", "smellfear.com", "snakemail.com", "sneakemail.com", "snkmail.com", "sofimail.com", "sofort-mail.de", "sogetthis.com", "soodonims.com", "spam.la", "spam.su", "spam4.me", "spamail.de", "spamarrest.com", "spamavert.com", "spambob.com", "spambob.net", "spambob.org", "spambog.com", "spambog.de", "spambog.ru", "spambox.info", "spambox.us", "spamcannon.com", "spamcannon.net", "spamcero.com", "spamcon.org", "spamcorptastic.com", "spamcowboy.com", "spamcowboy.net", "spamcowboy.org", "spamday.com", "spamex.com", "spamfree.eu", "spamfree24.com", "spamfree24.de", "spamfree24.eu", "spamfree24.net", "spamfree24.org", "spamgoes.in", "spamgourmet.com", "spamgourmet.net", "spamgourmet.org", "spamherelots.com", "spamhole.com", "spamify.com", "spaminator.de", "spamkill.info", "spaml.com", "spaml.de", "spammotel.com", "spamobox.com", "spamoff.de", "spamsalad.in", "spamslicer.com", "spamspot.com", "spamstack.net", "spamthis.co.uk", "spamthisplease.com", "spamtrail.com", "spamtroll.net", "speed.1s.fr", "spoofmail.de", "stinkefinger.net", "stop-my-spam.com", "streetwisemail.com", "stuffmail.de", "suioe.com", "supergreatmail.com", "supermailer.jp", "superrito.com", "superstachel.de", "suremail.info", "svk.jp", "sweetxxx.de", "tafmail.com", "tagyourself.com", "talkinator.com", "tapchicuoihoi.com", "teewars.org", "teleworm.com", "teleworm.us", "temp-mail.io", "temp-mail.org", "temp.emeraldwebmail.com", "tempail.com", "tempalias.com", "tempe-mail.com", "tempemail.biz", "tempemail.co", "tempemail.co.za", "tempemail.com", "tempemail.net", "tempinbox.co.uk", "tempinbox.com", "tempmail.com", "tempmail2.com", "tempmailaddress.com", "tempmaildemo.com", "tempmailer.com", "tempmailer.de", "tempmailo.com", "tempomail.fr", "temporarily.de", "temporarioemail.com.br", "temporaryemail.net", "temporaryemail.us", "temporaryforwarding.com", "temporaryinbox.com", "tempr.email", "tempymail.com", "thanksnospam.info", "thankyou-2010.com", "thankyou2010.com", "thecloudindex.com", "thisisnotmyrealemail.com", "throwam.com", "throwawayemailaddress.com", "throwawayemailaddresses.com", "throwawaymail.com", "tilien.com", "tittbit.in", "tmail.ws", "tmailinator.com", "tmpmail.net", "tmpmail.org", "toiea.com", "tokenmail.de", "toomail.biz", "topranklist.de", "tradermail.info", "trash-amil.com", "trash-mail.at", "trash-mail.com", "trash-mail.de", "trash2009.com", "trash2010.com", "trash2011.com", "trashcanmail.com", "trashdevil.com", "trashemail.de", "trashinbox.com", "trashmail.at", "trashmail.com", "trashmail.de", "trashmail.io", "trashmail.me", "trashmail.net", "trashmail.ws", "trashmailer.com", "trashymail.com", "trashymail.net", "trbvm.com", "trickmail.net", "trillianpro.com", "turual.com", "twinmail.de", "tyldd.com", "uggsrock.com", "umail.net", "unmail.ru", "upliftnow.com", "uplipht.com", "uroid.com", "us.af", "venompen.com", "veryrealemail.com", "vidchart.com", "viditag.com", "viewcastmedia.com", "viewcastmedia.net", "viewcastmedia.org", "vomoto.com", "vpn.st", "vsimcard.com", "vubby.com", "wasteland.rfc822.org", "webemail.me", "weg-werf-email.de", "wegwerfadresse.de", "wegwerfemail.de", "wegwerfmail.de", "wegwerfmail.info", "wegwerfmail.net", "wegwerfmail.org", "wetrainbayarea.com", "wetrainbayarea.org", "wh4f.org", "whatpaas.com", "whopy.com", "whyspam.me", "wickmail.net", "wilemail.com", "willhackforfood.biz", "willselfdestruct.com", "winemaven.info", "wronghead.com", "wuzup.net", "wuzupmail.net", "www.e4ward.com", "www.gishpuppy.com", "www.mailinator.com", "wwwnew.eu", "xagloo.com", "xemaps.com", "xents.com", "xmaily.com", "xoxy.net", "yapped.net", "yeah.net", "yep.it", "yogamaven.com", "yopmail.co.il", "yopmail.com", "yopmail.fr", "yopmail.net", "yourdomain.com", "ypmail.webarnak.fr.eu.org", "yuurok.com", "zehnminuten.de", "zehnminutenmail.de", "zippymail.info", "zoaxe.com", "zoemail.com", "zoemail.net", "zoemail.org"]);
+
+const EMAIL_SHAPE_RE = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+export async function onRequestOptions() {
+  return new Response(null, { headers: corsHeaders() });
+}
+
+export async function onRequestGet(context) {
+  const { request, env } = context;
+  const clientIP = request.headers.get('CF-Connecting-IP') || 'unknown';
+
+  const allowed = await checkRateLimit(env, clientIP, 'emaildisposable');
+  if (!allowed) {
+    return new Response(JSON.stringify({ error: 'Rate limit exceeded. Max 60 requests per minute.' }), {
+      status: 429, headers: { 'Content-Type': 'application/json', ...corsHeaders() },
+    });
+  }
+
+  const { searchParams } = new URL(request.url);
+  const email = (searchParams.get('email') || '').trim();
+
+  if (!email) {
+    return new Response(JSON.stringify({
+      usage: 'GET /api/email-disposable?email=foo@example.com',
+      known_domains_count: DISPOSABLE_DOMAINS.size,
+    }, null, 2), { headers: { 'Content-Type': 'application/json', ...corsHeaders() } });
+  }
+  if (!EMAIL_SHAPE_RE.test(email)) {
+    return new Response(JSON.stringify({ error: 'Invalid email shape' }), {
+      status: 400, headers: { 'Content-Type': 'application/json', ...corsHeaders() },
+    });
+  }
+
+  const domain = email.split('@')[1].toLowerCase();
+  const isDisposable = DISPOSABLE_DOMAINS.has(domain);
+
+  return new Response(JSON.stringify({
+    email,
+    domain,
+    disposable: isDisposable,
+  }), {
+    headers: { 'Content-Type': 'application/json', 'Cache-Control': 'public, max-age=3600', ...corsHeaders() },
+  });
+}
---
+++ functions/api/password-breach.js
@@ -0,0 +1,96 @@
+// GET /api/password-breach?password=...
+// Utilise le pattern k-anonymity de HIBP : seuls les 5 premiers caractères du hash SHA-1
+// sont envoyés à l'API externe. Le mot de passe complet ne quitte jamais l'edge Cloudflare
+// et n'est jamais loggé.
+
+async function checkRateLimit(env, clientIP, bucket) {
+  if (!env.PRESEND_ANALYTICS) return true;
+  const now = Math.floor(Date.now() / 60000);
+  const rateKey = `rate:${bucket}:${clientIP}:${now}`;
+  let count = await env.PRESEND_ANALYTICS.get(rateKey);
+  count = count ? parseInt(count) : 0;
+  if (count >= 20) return false;
+  await env.PRESEND_ANALYTICS.put(rateKey, (count + 1).toString(), { expirationTtl: 120 });
+  return true;
+}
+
+function corsHeaders(extra = {}) {
+  return { 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Methods': 'GET, OPTIONS', ...extra };
+}
+
+async function sha1Hex(text) {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(text);
+  const hashBuffer = await crypto.subtle.digest('SHA-1', data);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  return hashArray.map((b) => b.toString(16).padStart(2, '0')).join('').toUpperCase();
+}
+
+export async function onRequestOptions() {
+  return new Response(null, { headers: corsHeaders() });
+}
+
+export async function onRequestGet(context) {
+  const { request, env } = context;
+  const clientIP = request.headers.get('CF-Connecting-IP') || 'unknown';
+
+  const allowed = await checkRateLimit(env, clientIP, 'passwordbreach');
+  if (!allowed) {
+    return new Response(JSON.stringify({ error: 'Rate limit exceeded. Max 20 requests per minute.' }), {
+      status: 429, headers: { 'Content-Type': 'application/json', ...corsHeaders() },
+    });
+  }
+
+  const { searchParams } = new URL(request.url);
+  const password = searchParams.get('password');
+
+  if (!password) {
+    return new Response(JSON.stringify({
+      usage: 'GET /api/password-breach?password=...',
+      note: 'Uses k-anonymity: only the first 5 chars of the SHA-1 hash are sent externally. Your password is never logged or transmitted in full.',
+    }, null, 2), { headers: { 'Content-Type': 'application/json', ...corsHeaders() } });
+  }
+  if (password.length > 256) {
+    return new Response(JSON.stringify({ error: 'Password too long' }), {
+      status: 400, headers: { 'Content-Type': 'application/json', ...corsHeaders() },
+    });
+  }
+
+  try {
+    const hash = await sha1Hex(password);
+    const prefix = hash.slice(0, 5);
+    const suffix = hash.slice(5);
+
+    const controller = new AbortController();
+    const timeout = setTimeout(() => controller.abort(), 8000);
+
+    const res = await fetch(`https://api.pwnedpasswords.com/range/${prefix}`, {
+      headers: { 'Add-Padding': 'true' },
+      signal: controller.signal,
+    });
+    clearTimeout(timeout);
+
+    if (!res.ok) throw new Error(`HIBP API error: HTTP ${res.status}`);
+    const text = await res.text();
+
+    let breachCount = 0;
+    for (const line of text.split('\n')) {
+      const [lineSuffix, countStr] = line.trim().split(':');
+      if (lineSuffix === suffix) {
+        breachCount = parseInt(countStr, 10);
+        break;
+      }
+    }
+
+    return new Response(JSON.stringify({
+      breached: breachCount > 0,
+      breach_count: breachCount,
+    }), {
+      headers: { 'Content-Type': 'application/json', 'Cache-Control': 'no-store', ...corsHeaders() },
+    });
+  } catch (e) {
+    return new Response(JSON.stringify({ error: 'Could not check password right now.', detail: e.message }), {
+      status: 502, headers: { 'Content-Type': 'application/json', ...corsHeaders() },
+    });
+  }
+}
